6f663fe8f59f82d14c959dc25316d522be9846ab
Democrito <demohenry6@gmail.com>
2021-04-30 17:24:54 -0300

    commit boilerplate

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  testEnviromnment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
-}
+	testEnviromnment: 'jsdom',
+	testPathIgnorePatterns: ['/node_modules/', '/.next'],
+	collectCoverage: true,
+	collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+};
